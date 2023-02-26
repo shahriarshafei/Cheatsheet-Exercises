9239b04eff40e43ed9b9483a2b3d53e280b6ac80
shahriarshafei <125210205+shahriarshafei@users.noreply.github.com>
2023-02-26 13:20:07 +0330

    creating JavaScript cheat sheet and exercises

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Typing disciplin in Javascript
+    camelCasing = variables;
+    Pascal =
+
+// How to name variables
+    my_name, myName, my123;
+
+// Data types in Javascript
+    String, Number, Boolean
