23717f105b2a64b2546adbcbccf55dddf1906726
shahriarshafei <125210205+shahriarshafei@users.noreply.github.com>
2023-03-02 14:40:08 +0330

    update

--- javascript.js
+++ javascript.js
@@ -7,3 +7,12 @@
 
 // Data types in Javascript
     String, Number, Boolean
+
+// Creating new variable and new Object and function
+    var variable = "Something";
+    var variable = 123;
+    var object = new Object();                          //Creat a new object of Object
+    function doSomething(x){
+        var done = x+2
+        return done;
+    };           //To call function simply write doSomething(x);
