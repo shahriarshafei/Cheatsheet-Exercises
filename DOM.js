6ad4b3f235c27736db3259344a9169c52af37f80
shahriarshafei <125210205+shahriarshafei@users.noreply.github.com>
2023-03-05 17:14:37 +0330

    Adding some useful commands

--- DOM.js
+++ DOM.js
@@ -1,3 +1,21 @@
 // To use external javaScript in your website add <script src=""></script> to end of your body section of your html file
 // to use inline javaScript in your website use 'onload=""' in the attribute section of your html tag.
 // To use intrnal javaScript in your website user <script>""</script> anywhere on your html file.
+
+// useful DOM commands:
+// Select your document (your html page turned into DOM by browser an making a tree hierchy)
+    document;
+// Select first element child of your docmunet
+    document.firstElementChild;
+// Select last element child of your document
+    document.lastElementChild;
+// Changing the text inside an HTML tag, including text decoratives like <em> & <strong>...
+    document.innerHTML;
+// Changing ONLY the text inside of an HTML tag.
+    document.innerText;
+// Changing CSS rules of a selection
+    document.style;
+// Changing CSS color of a selection
+    document.style.color = "";
+// Returning document length.
+    document.length;
