0a1cd58860ae5bb9f5a80a9332d3382db6acf753
shahriarshafei <125210205+shahriarshafei@users.noreply.github.com>
2023-03-05 17:13:45 +0330

    creating DOM and basic instructions

---
+++ DOM.js
@@ -0,0 +1,3 @@
+// To use external javaScript in your website add <script src=""></script> to end of your body section of your html file
+// to use inline javaScript in your website use 'onload=""' in the attribute section of your html tag.
+// To use intrnal javaScript in your website user <script>""</script> anywhere on your html file.
