03b4d6f0c1fb2829c1230f5c90d45044b6f47e8a
shahriarshafei <125210205+shahriarshafei@users.noreply.github.com>
2023-03-11 14:40:44 +0330

    Explaining events of listeners

--- DOM.js
+++ DOM.js
@@ -76,3 +76,18 @@
     document.addEventListener("keydown", function(event){
         console.log(event)
     })
+    // To better understand this here is a hypothetical scenario:
+        function anotherAddEventListener(typeOfEvent, callBack){
+
+                //Detect Event Code...
+
+            var eventThatHappened = {
+                eventType: "keypress",
+                key: "p",
+                durationOfKeypress: 2,
+            }
+
+            if (eventThatHappened.eventType === typeOfEvent){
+                callBack(eventThatHappened);
+            }
+        }
