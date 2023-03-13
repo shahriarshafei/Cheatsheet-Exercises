4afeb59e56ed196e51ccdfa0efd2273004a32dd5
shahriarshafei <125210205+shahriarshafei@users.noreply.github.com>
2023-03-13 12:10:45 +0330

    Selecting elements using jQuery

---
+++ jQuery.js
@@ -0,0 +1,7 @@
+// Always add jQuery CDN on top of your javaScript link.
+
+// Selecting en element using jQuery.
+    //document.querySelector("h1"); using $ in jQuery is not similar to this
+    document.querySelectorAll|("h1");
+    jQuery("h1") //OR 
+    $("h1")
